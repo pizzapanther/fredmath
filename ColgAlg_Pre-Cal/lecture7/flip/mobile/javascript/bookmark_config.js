de2721b3046f9123bd642aeea8b19836ee074a55
fkhoury1 <32973894+fkhoury1@users.noreply.github.com>
2020-01-08 12:34:01 -0600

    Add files via upload

---
+++ ColgAlg_Pre-Cal/lecture7/flip/mobile/javascript/bookmark_config.js
@@ -0,0 +1,1 @@
+﻿var ols=[{caption:"sec-7.1",page:"0",url:"",level:"1",children:[]},{caption:"sec-7.2",page:"13",url:"",level:"1",children:[]},{caption:"sec-7.3",page:"18",url:"",level:"1",children:[]}];
