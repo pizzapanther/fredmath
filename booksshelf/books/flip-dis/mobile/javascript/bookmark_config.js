d13ace0a85dd6905da73a5fc2f8deb8f186633dd
fkhoury1 <32973894+fkhoury1@users.noreply.github.com>
2020-01-23 06:43:28 -0600

    Discrete
    Discretet updates

---
+++ booksshelf/books/flip-dis/mobile/javascript/bookmark_config.js
@@ -0,0 +1,1 @@
+﻿var ols=[{caption:"Discrete_lecture1",page:"1",url:"",level:"1",children:[]},{caption:"Discrete_lecture2",page:"87",url:"",level:"1",children:[]},{caption:"Discrete_Lecture3",page:"137",url:"",level:"1",children:[]},{caption:"Discrete_lecture4",page:"191",url:"",level:"1",children:[]}];
